23dbd28c3bfc6c8c1fc064b707a1444e07c4cba9
Suwanika <suwanika2002@gmail.com>
2026-03-10 15:25:56 +0530

    Initial Vue Product Explorer setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <div class="flex items-center justify-center h-screen">
+    <h1 class="text-4xl font-bold text-blue-500">
+      Tailwind is working 🚀
+    </h1>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style>
+</style>
